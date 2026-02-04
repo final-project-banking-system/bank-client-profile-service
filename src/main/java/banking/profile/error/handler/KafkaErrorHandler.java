2b3e612e15e221b0cb21651b649f42e0bc121805
francois202 <kakhmelev@miem.hse.ru>
2026-02-04 22:21:28 +0300

    dlt topics fix super final

--- src/main/java/banking/profile/error/handler/KafkaErrorHandler.java
+++ src/main/java/banking/profile/error/handler/KafkaErrorHandler.java
@@ -3,6 +3,7 @@ package banking.profile.error.handler;
 import banking.profile.error.exception.ClientProfileNotFoundException;
 import com.fasterxml.jackson.core.JsonParseException;
 import org.apache.kafka.common.TopicPartition;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
@@ -11,12 +12,14 @@ import org.springframework.kafka.support.ExponentialBackOffWithMaxRetries;
 
 @Configuration
 public class KafkaErrorHandler extends DefaultErrorHandler {
-    public KafkaErrorHandler(KafkaTemplate<String, String> kafkaTemplate) {
+    public KafkaErrorHandler(KafkaTemplate<String, String> kafkaTemplate,
+                             @Value("${spring.kafka.consumer.group-id}")
+                             String consumerGroupId) {
         super(
                 new DeadLetterPublishingRecoverer(
                         kafkaTemplate,
                         (record, ex) -> new TopicPartition(
-                                record.topic() + ".dq", record.partition())
+                                record.topic() + "." + consumerGroupId + ".dq", record.partition())
                 ),
                 new ExponentialBackOffWithMaxRetries(3)
         );
